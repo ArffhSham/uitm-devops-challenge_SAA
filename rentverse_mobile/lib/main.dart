6dfdfb9df66bae50d2a962972d82809d3c2a3da6
qiila-hub <2024779467@student.uitm.edu.my>
2025-12-17 11:49:13 +0800

    main

---
+++ rentverse_mobile/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'features/auth/ui/login_screen.dart';
+
+void main() {
+  runApp(const ProviderScope(child: RentVerseApp()));
+}
+
+class RentVerseApp extends StatelessWidget {
+  const RentVerseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
